6e374ec4a35ed73d59425168f62f3ed6bef39c7d
Ganapathy Chidambaram <gchidambaram@walmart.com>
2014-05-23 02:30:53 -0700

    1st

---
+++ src/Assign1/CountHolder.java
@@ -0,0 +1,17 @@
+package Assign1;
+//
+public class CountHolder {
+	
+	public long numSplitInv = 0;
+	public int[] mergedList = null;
+	
+	public CountHolder(){
+		
+	}
+	
+	public CountHolder(long val, int[] arr){
+		numSplitInv = val;
+		mergedList = arr;
+	}
+
+}
---
+++ src/Assign1/CountInversions.java
@@ -0,0 +1,118 @@
+package Assign1;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class CountInversions {
+	
+	public static List<Integer> readFromFile(String fileName) throws FileNotFoundException{
+		
+		Scanner scanner = new Scanner(new File(fileName));
+		List<Integer> output = new ArrayList<Integer>();
+		int i = 0;
+		while(scanner.hasNextInt())
+		{
+		     output.add(scanner.nextInt());
+		}
+		
+		return output;
+	}
+	
+	public static long bruteCount(List<Integer> input){
+		
+	   long outCount = 0;
+	   
+	   for(int i = 0; i < input.size()-1; i++)
+	   {
+		   int firstVal = input.get(i);
+		   
+		   for(int j = i+1; j < input.size();j++)
+		   {
+			   int secondVal = input.get(j);
+			   
+			   if (firstVal > secondVal)
+				   outCount++;
+		   }
+		   
+	   }
+	   
+	   
+	   return outCount;
+	}
+	
+	public static CountHolder divConCount(List<Integer> input,int start, int end){
+		
+		if(start == end)
+			return new CountHolder(0,new int[]{input.get(start)});
+		else
+		{
+			int mid = (start + end)/2;
+			
+			CountHolder left = divConCount(input,start,mid);
+			CountHolder right = divConCount(input,mid+1,end);
+			
+			CountHolder output = mergeAndCountSplits(left.mergedList,right.mergedList);
+			
+			output.numSplitInv = output.numSplitInv + left.numSplitInv + right.numSplitInv;
+			
+			return output;
+			
+		}
+		
+	
+		
+	}
+	
+	private static CountHolder mergeAndCountSplits(int[] left,
+			int[] right) {
+		
+		CountHolder output = new CountHolder(0,new int[left.length + right.length]);
+		int i=0,j= 0,k = 0;
+		
+		while(i < left.length && j <right.length)
+		{
+			if(left[i] <= right[j])
+				output.mergedList[k++] = left[i++];
+			else
+			{
+				output.mergedList[k++] = right[j++];
+				output.numSplitInv = output.numSplitInv + left.length - i;
+			}
+		}
+		
+		
+		while (i < left.length)
+				output.mergedList[k++] = left[i++];
+		
+		while(j < right.length)
+			  output.mergedList[k++] = right[j++];
+	
+			
+		
+		return output;
+	}
+
+	public static void main(String[] args){
+		System.out.println("hi");
+		try 
+		{
+			List<Integer> numList = readFromFile("/Users/Ramanan/Algorithms/Assignments/src/IntegerArray.txt");
+			System.out.println("length of list = "+numList.size());
+			long start = System.currentTimeMillis();
+			long numInversions = bruteCount(numList);
+			long middle = System.currentTimeMillis();			
+			CountHolder holder = divConCount(numList,0,numList.size()-1);
+			long end = System.currentTimeMillis();	
+			
+			System.out.println("Brute count answer = "+numInversions+", time taken = "+(middle -start));
+			System.out.println("Divide and conauer = "+holder.numSplitInv+", time taken = "+(end - middle));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
