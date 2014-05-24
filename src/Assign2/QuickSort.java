99bcb55a16b95c14099c4afb9279d93f52e56e7a
Ganapathy Chidambaram <gchidambaram@walmart.com>
2014-05-24 17:12:31 +0530

    assignment 2

--- src/Assign2/QuickSort.java
+++ src/Assign2/QuickSort.java
@@ -14,11 +14,11 @@ public class QuickSort {
 		{
 			int retValue = end - start;
 			
-			int partIndex = endPartition (inList,start,end);
+			int partIndex = partition (inList,start,end);
 			
 
 			retValue = retValue + sortAndCount(inList,start,partIndex-1);			
-			retValue = retValue + sortAndCount(inList,partIndex,end);
+			retValue = retValue + sortAndCount(inList,partIndex+1,end);
 
 			
 			return retValue;
@@ -27,13 +27,7 @@ public class QuickSort {
 		
 	}
 	
-	private void displayArray(List<Integer> inList,int start,int end){
-		
-		
-			System.out.println(" "+start+" "+end);
-		
-		
-	}
+
 
 	private int partition(List<Integer> inList,int start,int end){
 		
@@ -54,14 +48,17 @@ public class QuickSort {
 		
 		swap(inList,start,i-1);
 		
-		return i;
+		return i-1;
 		
 	}	
 
-	
-	private int endPartition(List<Integer> inList,int start,int end){
+	private int medianOf3Partition(List<Integer> inList,int start,int end){
 		
-		swap(inList,start,end-1);
+		int middleIndex = (start + end)/2;
+		
+		int medianIndex = findMedian(inList,start,end,middleIndex);
+		
+		swap(inList,start,medianIndex);
 		
 		int p = inList.get(start);
 		int i = start + 1;
@@ -78,7 +75,48 @@ public class QuickSort {
 		
 		swap(inList,start,i-1);
 		
-		return i;
+		return i-1;
+		
+	}	
+	
+	private int findMedian(List<Integer> inList, int start, int end,
+			int middleIndex) {
+		
+		int max = Math.max(inList.get(middleIndex),Math.max(inList.get(start), inList.get(end)));
+		
+		int min = Math.min(inList.get(middleIndex),Math.min(inList.get(start), inList.get(end)));
+		
+		if(inList.get(start)!=max && inList.get(start)!=min)
+			return start;
+		else if(inList.get(end)!=max && inList.get(end)!=min)
+			return end;
+		else
+			return middleIndex;
+		
+	}
+
+
+
+	private int endPartition(List<Integer> inList,int start,int end){
+		
+		swap(inList,start,end);
+		
+		int p = inList.get(start);
+		int i = start + 1;
+		
+		for(int j = start + 1;j<=end;j++)
+		{
+			if(inList.get(j) < p)
+			{
+				swap(inList,i,j);
+				i++;
+			}		
+			
+		}
+		
+		swap(inList,start,i-1);
+		
+		return i-1;
 		
 	}
 
